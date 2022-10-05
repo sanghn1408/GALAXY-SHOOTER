657f4c5bcdb52590f0b3e38bc6923e2fd126216b
sanghn1408 <sanghn1408@gmail.com>
2022-10-06 00:28:45 +0700

    commit3
    newinputsystem

---
+++ GALAXY SHOOTER/Assets/PlayerInput.cs
@@ -0,0 +1,293 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerInput.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerInput"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""233e5fa2-e6d3-4270-9919-dd72d40e50e5"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""e034b08b-f313-43b4-8529-4f2a6ebc4cb2"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Attack"",
+                    ""type"": ""Button"",
+                    ""id"": ""e7f5e441-9804-489c-a404-f4a1db1c6fd3"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""9a62ec3f-e816-4514-b475-63140fc49be6"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""d9c65d06-b6f9-4218-9f63-7c86fc672371"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""24fdcd10-5b3b-4a4b-8e6d-472b55617479"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""8366f418-e20a-4e14-bc13-81ac4e24fcad"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""8d1c7e70-6a47-40ad-bc98-3081c0efcb94"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Arrow"",
+                    ""id"": ""8124b58b-47d7-4016-9236-1d80a8897012"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""c02f63fc-82f6-4c17-9d69-4270b87329d0"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""ac5e47a5-a370-4c2d-b14e-a01483732694"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""f5a0cfb0-04ca-4555-a63a-3b1c51e33eb5"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""7b9be96d-1411-43fc-9da8-400e8409e1c6"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9d150658-d2b4-4430-87bd-b701e32d6fe0"",
+                    ""path"": ""<Joystick>/stick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d5d5a366-c18a-4d79-b754-9f2ab0a96e00"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9941ac7c-43e4-477a-b852-a0d6c75d24ee"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d2e0d0aa-3365-4a53-8f3b-75f35066c3b9"",
+                    ""path"": ""<Touchscreen>/press"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Attack"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+        m_Player_Attack = m_Player.FindAction("Attack", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Movement;
+    private readonly InputAction m_Player_Attack;
+    public struct PlayerActions
+    {
+        private @PlayerInput m_Wrapper;
+        public PlayerActions(@PlayerInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputAction @Attack => m_Wrapper.m_Player_Attack;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Attack.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack;
+                @Attack.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack;
+                @Attack.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnAttack;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Attack.started += instance.OnAttack;
+                @Attack.performed += instance.OnAttack;
+                @Attack.canceled += instance.OnAttack;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnAttack(InputAction.CallbackContext context);
+    }
+}
--- GALAXY SHOOTER/Assets/Scripts/PlayerController.cs
+++ GALAXY SHOOTER/Assets/Scripts/PlayerController.cs
@@ -13,14 +13,39 @@ using UnityEngine;
         [SerializeField] private Transform m_FiringPoint;
         [SerializeField] private float m_FiringCooldown;
         [SerializeField] private int m_Hp;
+        [SerializeField] private bool m_UseNewInputSystem;
 
         private int m_CurrentHp;
         private float m_TempCooldown;
+        private PlayerInput m_PlayerInput;
+        private Vector2 m_MovementInputValue;
+        private bool m_AttackInputValue;
+
+    private void OnEnable()
+    {
+        if(m_PlayerInput == null)
+        {
+            m_PlayerInput = new PlayerInput();
+            m_PlayerInput.Player.Movement.started += OnMovement;
+            m_PlayerInput.Player.Movement.performed += OnMovement;
+            m_PlayerInput.Player.Movement.canceled += OnMovement;
+            m_PlayerInput.Player.Attack.started += OnAttack;
+            m_PlayerInput.Player.Attack.performed += OnAttack;
+            m_PlayerInput.Player.Attack.canceled += OnAttack;
+            m_PlayerInput.Enable();
+
+        }
+    }
+
+    private void OnDisable()
+    {
+        m_PlayerInput.Disable();
+    }
     //private SpawnManager m_SpawnManager;
     //private GameManager m_GameManager;
     //private AudioManager m_AudioManager;
-    
-    // Start is called before the first frame update
+
+
     void Start()
         {
         m_CurrentHp = m_Hp;
@@ -41,11 +66,12 @@ using UnityEngine;
             {
                 return; 
             }
+            Vector2 direction = Vector2.zero;
+            if (!m_UseNewInputSystem)
+            {
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
-            Vector2 direction = new Vector2(horizontal, vertical);
-            transform.Translate(direction * Time.deltaTime * m_MoveSpeed);
-
+             direction = new Vector2(horizontal, vertical);
             if (Input.GetKey(KeyCode.Space))
             {
                 if (m_TempCooldown <= 0)
@@ -54,6 +80,21 @@ using UnityEngine;
                     m_TempCooldown = m_FiringCooldown;
                 }
             }
+        }
+            else
+            {
+                 direction = m_MovementInputValue;
+
+                 if(m_AttackInputValue)
+                 {
+                    if (m_TempCooldown <= 0)
+                    {
+                        Fire();
+                        m_TempCooldown = m_FiringCooldown;
+                    }
+                 }
+            }
+            transform.Translate(direction * Time.deltaTime * m_MoveSpeed); 
 
             m_TempCooldown -= Time.deltaTime;
         }
@@ -85,4 +126,37 @@ using UnityEngine;
             }
          AudioManager.Instance.PlayHitSFX();
         }
+    private void OnAttack(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        if(obj.started)
+        {
+            m_AttackInputValue = true;
+        }
+        else if(obj.performed)
+        {
+            m_AttackInputValue = true;
+        }
+        else if(obj.canceled)
+        {
+            m_AttackInputValue = false;
+        }
     }
+
+    private void OnMovement(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+
+        if (obj.started)
+        {
+            m_MovementInputValue = obj.ReadValue<Vector2>();
+        }
+        else if (obj.performed)
+        {
+            m_MovementInputValue = obj.ReadValue<Vector2>();
+        }
+        else if (obj.canceled)
+        {
+            m_MovementInputValue = Vector2.zero;
+        }
+
+    }
+}
